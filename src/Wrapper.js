a20adc19f6d9cd8ba7944882a585e1049a742a32
asraful196 <asraful.01110@gmail.com>
2022-12-12 18:10:59 +0600

    hoc with react-router

---
+++ src/Wrapper.js
@@ -0,0 +1,11 @@
+const wrapper =
+  (Component) =>
+  ({ ...props }) =>
+    (
+      <div style={{ border: "2px solid blue", borderRadius: "3px" }}>
+        <h1>This is the wrapper content</h1>
+        <Component {...props} />
+      </div>
+    );
+
+export default wrapper;
