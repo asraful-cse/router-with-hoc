85769fd2c66ea6a3f134a3459ebd9fdeadf06095
asraful196 <asraful.01110@gmail.com>
2022-12-12 18:39:40 +0600

    router with hoc some code updated

--- src/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Welcome() {
-  return (
-    <div style={{ border: "2px dotted green", borderRadius: "3px" }}>
-      <h3>Welcome to your React App</h3>
-    </div>
-  )
-}
